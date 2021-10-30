7aea1218d23de40061298e23cc06e2186124c98a
Steve Ruiz <steveruizok@gmail.com>
2021-10-30 20:12:22 +0100

    Filter bound arrows from snaps

--- example-advanced/src/state/machine.ts
+++ example-advanced/src/state/machine.ts
@@ -316,10 +316,15 @@ export const state = createState({
       const all: TLBoundsWithCenter[] = []
       const others: TLBoundsWithCenter[] = []
 
+      const bindings = Object.values(data.page.bindings)
+
       Object.values(data.page.shapes).forEach((shape) => {
         const bounds = Utils.getBoundsWithCenter(getShapeUtils(shape).getRotatedBounds(shape))
         all.push(bounds)
-        if (!data.pageState.selectedIds.includes(shape.id)) {
+        if (
+          !data.pageState.selectedIds.includes(shape.id) &&
+          !bindings.find((binding) => binding.fromId === shape.id)
+        ) {
           others.push(bounds)
         }
       })
